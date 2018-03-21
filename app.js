b0aab68b88bafb0a34ef31a938795bea8ef3a446
eldiegomoustachu <31035533+eldiegomoustachu@users.noreply.github.com>
2018-03-21 15:54:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,19 +6,22 @@ var prefix = '?';
 
 client.on("message", message => {
 
-  // Le robot lui répondra pong si il envoi ping
+  // Le robot lui répondra pong si il envoie "ping"
   if (message.content === "ping"){
-    // le robot répond pong !
+    // le robot répond "Pong !"
     message.channel.send("Pong !");
   };
-
-
-  // Le robot lui répondra ceci si un utilisateur fait ?help
+  // Le robot lui répondra ceci si un utilisateur fait "?help"
   if (message.content.startsWith(prefix + "help")) {
     message.channel.send("Vous avez fait `?help` pour avoir l'aide.");
   };
+  // Le robot lui répondra si l'utilisateur envoie "mp"
   if (message.content == "mp"){
     message.author.send("voici un message privé !")
+  };
+   // Le robot lui répondra ceci si un utilisateur envoie "?avatar"
+  if (message.content.startsWith(prefix + "avatar")) {
+    message.channel.send(message.author.avatarURL);
   };
 
 })
